ebe45d1003ee3ee7410aa584e34098a7a74b53f8
Niko <ks2225@protonmail.com>
2022-11-27 04:40:45 +0200

    base: whole bunch of changes
    Uploading files actually works now.
    Extensions on the other hand....
    We'll see what I can do later

--- nim_file_uploader.nimble
+++ nim_file_uploader.nimble
@@ -5,9 +5,11 @@ author        = "Niko"
 description   = "File uploader for Chatterino2 "
 license       = "Apache-2.0"
 srcDir        = "src"
-bin           = @["nim_file_uploader"]
+bin           = @["app"]
 
 
 # Dependencies
 
 requires "nim >= 1.6.8"
+requires "prologue 0.6.4"
+requires "imghdr 2.0"
--- src/app.nim
+++ src/app.nim
@@ -1,8 +1,24 @@
 import prologue
+import std/strformat
+import std/sha1
+
 
 proc hello*(ctx: Context) {.async.} =
-  resp "<h1>Hello, Prologue!</h1>"
+  resp "<h1>No, you <strong>won't</strong> find easter eggs here.</h1>"
+
+proc upload(ctx: Context) {.async.} =
+  if ctx.request.reqMethod == HttpGet:
+    await ctx.staticFileResponse("static/uploader.html", "")
+  elif ctx.request.reqMethod == HttpPost:
+
+    let file = ctx.getUploadFile("file")
+    let hash = secureHash(file.filename)
+
+    file.save("files/", fmt"{hash}")
+    resp fmt"<html><h1>{file.filename}</h1><p>{file.body}</p></html>"
 
 let app = newApp()
 app.get("/", hello)
+app.addRoute("/upload", upload, @[HttpGet, HttpPost])
+
 app.run()
